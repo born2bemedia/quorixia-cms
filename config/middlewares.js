d7d3b44ca2bbae4d86d54501127b53bd254e4185
SergWebAcceptance <serg.web@acceptance.ltd>
2024-09-23 11:20:09 +0300

    added config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,22 @@
-module.exports = [
-  'strapi::logger',
+module.exports = ({ env }) => [
+  // Other middlewares...
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'https://quorixia.com'], // Replace with your frontend URLs
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true, // Include if you need to send cookies with requests
+    },
+  },
 ];
