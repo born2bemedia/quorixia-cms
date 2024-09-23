bcd63d60a5f949a991057d3e299979141bd718da
SergWebAcceptance <serg.web@acceptance.ltd>
2024-09-23 19:34:35 +0300

    Added nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,21 @@
-module.exports = () => ({});
+// config/plugins.js
+
+module.exports = ({ env }) => ({
+    email: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.gmail.com"),
+        port: env.int("SMTP_PORT", 587), // Use 465 for secure connection or 587 for startTLS
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        // Use secure connection for Gmail
+        secure: env.bool("SMTP_SECURE", true),
+      },
+      settings: {
+        defaultFrom: env("SMTP_DEFAULT_FROM", "noreply@quorixia.com"),
+        defaultReplyTo: env("SMTP_DEFAULT_REPLY_TO", "noreply@quorixia.com"),
+      },
+    },
+  });
