5d9d6f896bd44ab2c94c106db0630c3b3b599a3b
SergWebAcceptance <serg.web@acceptance.ltd>
2024-09-24 19:21:55 +0300

    added uploads path

--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,28 @@
 module.exports = ({ env }) => ({
-    // ...
-    email: {
-      config: {
-        provider: 'nodemailer',
-        providerOptions: {
-          host: env('SMTP_HOST', 'smtp.gmail.com'),
-          port: env('SMTP_PORT', 587),
-          auth: {
-            user: env('SMTP_USERNAME'),
-            pass: env('SMTP_PASSWORD'),
-          },
-          secure: env('SMTP_SECURE', false),
+  // ...
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.gmail.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
         },
-        settings: {
-          defaultFrom: 'noreply@quorixia.com',
-          defaultReplyTo: 'noreply@quorixia.com',
-        },
+        secure: env("SMTP_SECURE", false),
+      },
+      settings: {
+        defaultFrom: "noreply@quorixia.com",
+        defaultReplyTo: "noreply@quorixia.com",
       },
     },
-    // ...
-  });
+  },
+  upload: {
+    provider: "local",
+    providerOptions: {
+      sizeLimit: 1000000, // Adjust this limit if needed (in bytes)
+      path: "/var/data/uploads", // Use the mount path from Render
+    },
+  },
+});
